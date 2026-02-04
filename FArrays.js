e2b96412eda05a34afb8dc858b93e935af17c92f
esbny <torresyela11@gmail.com>
2026-02-04 18:17:48 -0500

    avance antes de irme de la U

---
+++ FArrays.js
@@ -0,0 +1,14 @@
+
+//length
+let panaderia = ["pan","galletas","papas", "buñuelos","pandebonos","alpin de chocolate"]
+
+let numProductos = panaderia.length
+console.log("el numero disponible de productos de la panaderia es de "+ numProductos)
+
+//at
+ const numeros = [5, 12, 8, 130, 44,69,67];
+ let index = 3
+console.log(`el numero en la posicion ${index} en el arreglo de numero es ${numeros.at(index)} `)
+
+index = -2
+console.log(`el numero en la posicion ${index} en el arreglo de numero es ${numeros.at(index)} `)
